be8d5ecaae2aefbe331cb85a95365fb1083d742d
EmileLin01 <136859577+EmileLin01@users.noreply.github.com>
2025-05-27 13:48:10 -0400

    Add files via upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Environmental",p:["p1"]},{w:"Hazards",p:["p1"]},{w:"Equipment",p:["p2"]},{w:"and",p:["p2"]},{w:"Tools",p:["p2"]},{w:"Essential",p:["p3"]},{w:"Survival",p:["p3"]},{w:"Skills",p:["p3"]},{w:"Introduction",p:["p4"]},{w:"Jobs",p:["p5"]},{w:"in",p:["p5"]},{w:"a",p:["p5","p7"]},{w:"Post-Apocalyptic",p:["p5"]},{w:"World",p:["p5"]},{w:"Causes",p:["p6"]},{w:"of",p:["p6","p8"]},{w:"Mutation",p:["p6"]},{w:"Tips",p:["p7"]},{w:"for",p:["p7"]},{w:"Nomadic",p:["p7"]},{w:"Lifestyle",p:["p7"]},{w:"Types",p:["p8"]},{w:"Mutants",p:["p8"]},{w:"Understanding",p:["p9"]},{w:"New",p:["p9"]},{w:"Social",p:["p9"]},{w:"Structures",p:["p9"]},{w:"What",p:["p10"]},{w:"are",p:["p10"]},{w:"Mutants?",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6, 7];
+ph["p3"]=[8, 9, 10];
+ph["p4"]=[11];
+ph["p5"]=[12, 13, 14, 15, 16];
+ph["p6"]=[17, 18, 19];
+ph["p7"]=[20, 21, 14, 22, 23];
+ph["p8"]=[24, 18, 25];
+ph["p9"]=[26, 27, 28, 29];
+ph["p10"]=[30, 31, 32];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {2:-1,10:-1,8:-1,5:-1,7:-1,3:-1,6:-1,9:-1};
